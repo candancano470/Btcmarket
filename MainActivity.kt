623656cf9b12a4be50f57f1e1590d6741e294934
candancano470 <candancano470@gmail.com>
2026-04-27 12:26:22 +0300

    Update MainActivity.kt

--- MainActivity.kt
+++ MainActivity.kt
@@ -1,45 +1,5 @@
 package com.btcmorning.btcmarketpro
 
-import android.app.Activity
-import android.content.Intent
-import android.net.Uri
-import android.webkit.ValueCallback
-import android.webkit.WebChromeClient
-import android.webkit.WebView
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : FlutterActivity() {
-
-    private var fileChooserCallback: ValueCallback<Array<Uri>>? = null
-    private val FILE_CHOOSER_REQUEST = 1001
-
-    fun onShowFileChooser(
-        webView: WebView?,
-        filePathCallback: ValueCallback<Array<Uri>>,
-        fileChooserParams: WebChromeClient.FileChooserParams
-    ): Boolean {
-        fileChooserCallback?.onReceiveValue(null)
-        fileChooserCallback = filePathCallback
-
-        val intent = Intent(Intent.ACTION_PICK).apply {
-            type = "image/*"
-        }
-        startActivityForResult(
-            Intent.createChooser(intent, "Select Image"),
-            FILE_CHOOSER_REQUEST
-        )
-        return true
-    }
-
-    @Suppress("DEPRECATION")
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == FILE_CHOOSER_REQUEST) {
-            val results = if (resultCode == Activity.RESULT_OK && data?.data != null) {
-                arrayOf(data.data!!)
-            } else null
-            fileChooserCallback?.onReceiveValue(results)
-            fileChooserCallback = null
-        }
-    }
-}
+class MainActivity: FlutterActivity()
